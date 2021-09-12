a79b0fa7edb9dd3fb7aac0bf574ea443de5725c2
cha-han851 <65326647+cha-han851@users.noreply.github.com>
2021-09-12 22:37:54 +0900

    defaultValueに変更

--- client/src/components/account/SignUpConfirmation.jsx
+++ client/src/components/account/SignUpConfirmation.jsx
@@ -1,15 +1,22 @@
-import React, { useState} from 'react';
+import React from 'react';
 
 const SignUpConfirmationComponent = ( props ) => {
+        const first_name = props.first_name;
+        const last_name  = props.last_name;
+        const phone = props.phone;
+        const email = props.email;
+        const password = props.password;
+        const password_confirmation = props.password_confirmation;
+
     return(
         <div className="sign-up-confirmation">
             <h1>SignUpConfirmation</h1>
-                <p>姓<input type="text" name="first_name" value={props.first_name} /></p>
-                <p>名<input type="text" name="last_name"  value={props.last_name} /></p>
-                <p>電話番号<input type="text" name="phone" value={props.phone} /></p>
-                <p>Email<input type="text" name="email" value={props.email} /></p>
-                <p>パスワード<input type="password" name="password" value={props.password} /></p>
-                <p>確認用パスワード<input type="password" name="password_confirmation" value={props.password_confirmation} /></p>
+                <p>姓<input type="text" name="first_name" defaultValue={first_name} /></p>
+                <p>名<input type="text" name="last_name"  defaultValue={last_name} /></p>
+                <p>電話番号<input type="text" name="phone" defaultValue={phone} /></p>
+                <p>Email<input type="text" name="email" defaultValue={email} /></p>
+                <p>パスワード<input type="password" name="password" defaultValue={password} /></p>
+                <p>確認用パスワード<input type="password" name="password_confirmation" defaultValue={password_confirmation} /></p>
         </div>
     );
 }
